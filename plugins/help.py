2a03619a77a7fa21bb55a76f01dbe8bfc2f04c10
Shadowwzo <108619015+Shadowwzo@users.noreply.github.com>
2022-08-06 14:21:18 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Just Send me Any YouTube link ans i will download it for your in your favourable size. Join @Anime_lifee for watching anime. @Shadow_10_YT is my owner"
     await message.reply_text(helptxt)
